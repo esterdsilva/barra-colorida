33725f4e98d9cb0d2a73dd1cc3d88055cd63a329
esterdsilva <ester.dutra.silva@gmail.com>
2024-02-19 22:20:17 -0300

    normalização

--- App.js
+++ App.js
@@ -1,20 +1,71 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View } from 'react-native';
 
-export default function App() {
+function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View
+      style={{
+        backgroundColor: 'black',
+        width: '100%',
+        height: '100%',
+      }}>
+      <View
+        style={{
+          backgroundColor: 'white',
+          position: 'absolute',
+          width: '14%',
+          height: '70%',
+        }}></View>
+
+      <View
+        style={{
+          backgroundColor: 'yellow',
+          position: 'absolute',
+          width: '14%',
+          height: '70%',
+          left: '14%',
+        }}></View>
+      <View
+        style={{
+          backgroundColor: 'cyan',
+          position: 'absolute',
+          width: '14%',
+          height: '70%',
+          left: '28%',
+        }}></View>
+      <View
+        style={{
+          backgroundColor: 'red',
+          position: 'absolute',
+          width: '14%',
+          height: '70%',
+          left: '42%',
+        }}></View>
+      <View
+        style={{
+          backgroundColor: 'magenta',
+          position: 'absolute',
+          width: '14%',
+          height: '70%',
+          left: '55%',
+        }}></View>
+      <View
+        style={{
+          backgroundColor: 'blue',
+          position: 'absolute',
+          width: '14%',
+          height: '70%',
+          left: '68%',
+        }}></View>
+      <View
+        style={{
+          backgroundColor: 'green',
+          position: 'absolute',
+          width: '19%',
+          height: '70%',
+          left: '82%',
+        }}></View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
